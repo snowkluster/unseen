f5f6b388bab57b81adad5e55cbf384dabe627cd2
Rohan Kaushik <rohankaushik1@outlook.com>
2023-07-30 16:45:27 +0530

    directory structure completed

---
+++ TODO.md
@@ -0,0 +1,6 @@
+- [x] command line tool for hashing and detecting hashes
+- [ ] hash md5
+- [ ] hash sha1
+- [ ] hash sha2
+- [ ] hash sha256
+- [ ] detect hashes atleast 10
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+// md5 
+// sha1 
+// sha2
+// sha256 
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
 
 func main(){
 	fmt.Println("hashing tool")
